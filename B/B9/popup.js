1a76ff04c368c4f0ae85fe4de8917d44954a44b0
Shreyans Jain <shreyansj551@gmail.com>
2019-01-10 00:00:04 +0530

    Added panning to B7

--- B/B9/popup.js
+++ B/B9/popup.js
@@ -1,4 +1,4 @@
-chrome.tabs.onUpdated.addListener(function(){
+chrome.tabs.onUpdated.addListener(() => {
     chrome.tabs.query({active:true, currentWindow:true}, (tabs) => {
         var url = tabs[0].url;
         if (url == "https://bits-apogee.org/" || url == "https://bits-oasis.org/" || url == "https://bits-bosm.org/") {
